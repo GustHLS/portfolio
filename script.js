c03e67dcbaed1a7c070124a320c639abfaad8863
gusthls <gustavohls.work@hotmail.com>
2024-07-24 14:38:21 -0300

    Versão 0.0.1 do meu portfolio

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('https://api.github.com/users/gusthls/repos')
+        .then(response => response.json())
+        .then(data => {
+            const projectsDiv = document.getElementById('github-projects');
+            data.forEach(repo => {
+                const project = document.createElement('div');
+                project.classList.add('project');
+                if (repo.name != 'GustHLS'){
+                    project.innerHTML = `
+                        <h3><a href="${repo.html_url}" target="_blank">${repo.name}</a></h3>
+                        <p>${repo.description || 'No description provided'}</p>
+                    `;
+                    projectsDiv.appendChild(project);
+                }
+            });
+        })
+        .catch(error => console.error('Error fetching repos:', error));
+});
